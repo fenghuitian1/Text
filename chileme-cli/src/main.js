1629befadb3ba642fe6c1d6a743a4312cdc24557
Web-yuyu <in_yuyua@163.com>
2020-05-08 20:18:35 +0800

    添加iconfont以及重置样式

--- chileme-cli/src/main.js
+++ chileme-cli/src/main.js
@@ -8,7 +8,10 @@ import Axios from 'axios' //引入axios
 import ElementUI from 'element-ui';
 import 'element-ui/lib/theme-chalk/index.css';
 
+import './assets/style/style.css' //引入css的重置样式
+
 Vue.config.productionTip = false
+
 Vue.prototype.Axios = Axios   // 配置axios
 Vue.use(ElementUI); //配置elementUI
 
