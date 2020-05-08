584d2141c233e749cba2b3685e128c9149b4637a
Web-yuyu <in_yuyua@163.com>
2020-05-08 19:52:40 +0800

    新增框架elementUI

--- chileme-cli/src/main.js
+++ chileme-cli/src/main.js
@@ -4,9 +4,14 @@ import router from './router'
 import store from './store'
 import Axios from 'axios' //引入axios
 
-Vue.config.productionTip = false
+//引入elementUI
+import ElementUI from 'element-ui';
+import 'element-ui/lib/theme-chalk/index.css';
 
+Vue.config.productionTip = false
 Vue.prototype.Axios = Axios   // 配置axios
+Vue.use(ElementUI); //配置elementUI
+
 new Vue({
   router,
   store,
