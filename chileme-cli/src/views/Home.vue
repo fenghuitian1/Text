f4ad885506d8328b90eee3b83fcc4b0c544b2260
Web-yuyu <in_yuyua@163.com>
2020-05-09 07:45:47 +0800

    修改页面布局

--- chileme-cli/src/views/Home.vue
+++ chileme-cli/src/views/Home.vue
@@ -1,18 +1,33 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <el-row>
+      <el-col :span="2"><div class="grid-left grid-content bg-purple-dark"></div></el-col>
+      <el-col :span="22"><div class="grid-right grid-content bg-purple-dark"> <router-view/></div></el-col>
+    </el-row>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
 
 export default {
   name: 'Home',
-  components: {
-    HelloWorld
-  }
 }
 </script>
+<style lang="scss" scoped>
+@mixin init{
+  width: 100%;
+  height: 100%;
+  overflow: hidden;
+}
+
+.home{
+  @include init;
+  .grid-left{
+    background: rgb(53, 57, 235);
+  }
+  .el-row,.el-col,.grid-left,grid-right{
+    height: 100%;
+  }
+}
+
+</style>
---
+++ chileme-cli/src/views/Register.vue
@@ -0,0 +1,58 @@
+<template>
+  <div class="register">
+    <div class="el-icon-s-shop"></div>
+    <el-form
+      :model="form"
+      ref="Form"
+      label-width="100px"
+    >
+      <el-form-item
+        label="手机号"
+        prop="mobeli"
+        :rules="[
+                    { required: true, message: '手机号不能为空'},
+                    { type: 'number', message: '手机号必须为数字值'}
+                ]"
+      >
+        <el-input type="mobeli" v-model.number="form.mobeli" autocomplete="off"></el-input>
+      </el-form-item>
+      <el-form-item>
+        <el-button type="primary" @click="registerHandle('Form')">提交</el-button>
+        <el-button @click="resetForm('Form')">重置</el-button>
+      </el-form-item>
+    </el-form>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      //注册表单
+      form: {
+        mobeli:'',
+      }
+    };
+  },
+  methods: {
+    //提交注册表单方法
+     registerHandle(formName) {
+        this.$refs[formName].validate((valid) => {
+          if (valid) {
+            alert('submit!');
+          } else {
+            console.log('error submit!!');
+            return false;
+          }
+        });
+      },
+      resetForm(formName) {
+        this.$refs[formName].resetFields();
+      }
+  }
+};
+</script>
+
+<style lang="scss" scoped>
+
+</style>
