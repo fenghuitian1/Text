9f63ec3cc1399f2d3e3b382b0ca1f3615eb209ca
Web-yuyu <in_yuyua@163.com>
2020-05-12 14:33:59 +0800

    安装配置 koa-cors

--- chileme-sev/app.js
+++ chileme-sev/app.js
@@ -4,7 +4,7 @@ const cfg = require('./config')//引入配置文件
 const mongoose = require('mongoose')//引入mongoose
 const router = require('./router/router')
 const koaBody = require('koa-body') // 引入koa-body插件解析 post请求
-
+const koaCars = require('koa-cors') //引入koa-cors插件
 
 
 
@@ -19,6 +19,11 @@ mongoose.connection.once('open',()=>{   //once只触发一次
 })
 //async  声明函数是一个异步函数
 App.use(koaBody())  //配置 koa-body
+App.use(koaCars({
+    origin:'http://localhost:8080', //允许那个地址来访问我们的服务
+    credentials:true    //请求是否携带证书  cookie
+}))  //配置 koa-cars
+
 App.use(router.routes()).use(router.allowedMethods())// 启动路由
 
 
